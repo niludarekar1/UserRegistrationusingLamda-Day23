5bc22241f035737047aa5adb798149a6d6e2a575
niludarekar1 <nilu.darekar1@gmail.com>
2022-02-13 23:15:25 +0530

    Added:UserRegistrationUsingLamda

---
+++ src/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) throws UserRegistrationException {
+        System.out.println("Welcome to User Registration Program");
+        UserRegistration user = new UserRegistration();
+
+        System.out.println(user.validateFirstName());
+        System.out.println(user.validateLastName());
+        System.out.println(user.validateMailID());
+        System.out.println(user.validateMobileNumber());
+        System.out.println(user.validatePassword());
+    }
+}
